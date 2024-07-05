954e405c0a479d1cacc7e5397df5f709a174b95e
Javan Roundtree <169216679+Roundtree277@users.noreply.github.com>
2024-07-05 09:22:53 -0400

    rebuild is complete Any excess files that were not used was not included The models, views, controllers, and devise was regenerated using rails generate command Only thing that I'm unsure about is the admin dashboard controllers and views but I've included it in here Everything in this branch organized and easier know what file changes what

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum role: { student: 'student', instructor: 'instructor', admin: 'admin' }
+
+
+  validate :email_domain_check
+
+  private
+
+  def email_domain_check
+    unless email =~ /\A[\w+\-.]+@osu\.edu\z/i
+      errors.add(:email, 'should be an @osu.edu email address')
+    end
+  end
 end
