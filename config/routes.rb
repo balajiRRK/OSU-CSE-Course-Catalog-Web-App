0e70349d973dc840539187adcfe7b764e713bcb3
Javan Roundtree <169216679+Roundtree277@users.noreply.github.com>
2024-07-20 15:08:08 -0400

    Moved the add admin code from environments.rb to puma.rb with the help of https://stackoverflow.com/questions/37455021/how-to-set-puma-to-start-on-server-start-reboot-with-capistrano Fixed routing for logging out in the routes file and sorted out potential confilicts with the devise user route and the user route for the admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "home#index"
-  devise_for :users
-  resources :users , :path_prefix => 'admin'
+  devise_for :users, :path_prefix => 'accounts'
+  resources :users, :path_prefix => 'admin'
   resources :courses do
     collection do
     post :wipe
@@ -39,7 +39,7 @@ Rails.application.routes.draw do
 
 
   devise_scope :user do
-    get 'users/sign_out' => "devise/sessions#destroy"
+    get 'accounts/users/sign_out' => "devise/sessions#destroy"
   end
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
@@ -47,5 +47,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-   get '*path', to: 'errors#404'
+   # get '*path', to: 'errors#404'
 end
