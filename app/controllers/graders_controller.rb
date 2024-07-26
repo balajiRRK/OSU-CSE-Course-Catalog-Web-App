930b41e237815b9188a346e4e0a0dbd732a92e17
kash-01 <159463415+kash-01@users.noreply.github.com>
2024-07-26 08:50:12 -0400

    Update graders_controller.rb to include grader availability

--- app/controllers/graders_controller.rb
+++ app/controllers/graders_controller.rb
@@ -1,29 +1,71 @@
 class GradersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_grader, only: [:show, :edit, :update]
+
   def new
     @grader = Grader.new
+    # Initialize empty availability for all days
+    @grader.availability = Grader::DAYS_OF_WEEK.each_with_object({}) { |day, hash| hash[day] = ['', ''] }
   end
 
   def create
     @grader = Grader.new(grader_params)
+    process_availability
+    # Debug output to check availability after processing
+    puts "Availability after processing: #{@grader.availability.inspect}"
     if @grader.save
-      redirect_to @grader, notice: 'Grader application was succesfully submitted.'
+      # Debug output to check availability after save
+      puts "Availability after save: #{@grader.reload.availability.inspect}"
+      redirect_to @grader, notice: 'Grader application was successfully submitted.'
     else
       render :new
     end
   end
 
   def show
-    @grader = Grader.find(params[:id])
   end
 
   def index
     @graders = Grader.all
   end
 
+  def edit
+  end
+
+  def update
+    process_availability
+    # Debug output to check availability after processing
+    puts "Availability after processing: #{@grader.availability.inspect}"
+    if @grader.update(grader_params)
+      # Debug output to check availability after update
+      puts "Availability after update: #{@grader.reload.availability.inspect}"
+      redirect_to @grader, notice: 'Grader application was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
   private
 
+  def set_grader
+    @grader = Grader.find(params[:id])
+  end
+
   def grader_params
-    params.require(:grader).permit(:name, :subject, :email, :phone_number, :courses_wish_to_grade, :courses_qualified_to_grade)
+    params.require(:grader).permit(
+      :name, :email, :phone_number, :courses_wish_to_grade, :courses_qualified_to_grade,
+      availability: {}
+    )
+  end
+
+  def process_availability
+    return unless params[:grader][:availability]
+    
+    # Transform the availability hash to remove blank entries
+    @grader.availability = params[:grader][:availability].to_h.transform_values do |times|
+      times.map(&:presence).compact
+    end
+    # Debug output to check processed availability
+    puts "Processed availability: #{@grader.availability.inspect}"
   end
 end
