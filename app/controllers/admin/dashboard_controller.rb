4c9aed719c305f887a647db77fec4b3f75fe1ebb
Javan Roundtree <169216679+Roundtree277@users.noreply.github.com>
2024-07-29 22:29:31 -0400

    Prepared admin dashboard for graders application approval with another option to see applications

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,13 +3,14 @@ class Admin::DashboardController < ApplicationController
   include Devise
 
   def index
-    # @courses = Course.all
+    # paginations for the renders
     @pagy_assigned, @has_grader = pagy(Section.where.associated(:assistants).all, page_param: :has_grader)
     @pagy_unassigned, @no_grader = pagy(Section.where.missing(:assistants).all, page_param: :no_grader)
     @pagy_course, @courses = pagy(Course.all, page_param: :course_page)
     @pagy_user, @users = pagy(User.all, page_param: :user_page)
     @pagy_pending, @pending_users = pagy(User.where(status: "pending").where.not(role: "student").all, page_param: :pending_page)
     @pagy_assistant, @assistants = pagy(Assistant.all, page_param: :assistants)
+    @pagy_application, @grader = pagy(Grader.all)
 
     @assistant = Assistant.new
   end
