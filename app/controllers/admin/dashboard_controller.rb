8532991ee19db5cc9a7f8bd08092b2478af7e02e
Javan Roundtree <169216679+Roundtree277@users.noreply.github.com>
2024-07-29 20:24:58 -0400

    Pending users only shows and counts admin and instructors

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,14 +1,15 @@
 class Admin::DashboardController < ApplicationController
   before_action :authenticate_admin!, :authenticate_status!
   include Devise
+
   def index
-      # @courses = Course.all
-      @pagy_unassigned, @no_grader = pagy(Section.where.missing(:assistants).all, page_param: :no_grader)
-      @pagy_course, @courses = pagy(Course.all,page_param: :course_page)
-      @pagy_user, @users = pagy(User.all,page_param: :user_page)
-      @pagy_pending, @pending_users = pagy( User.where(status: "pending").all,page_param: :pending_page)
-      @pagy_assistant, @assistants = pagy(Assistant.all,page_param: :assistants)
-      
-      @assistant = Assistant.new
-    end
+    # @courses = Course.all
+    @pagy_unassigned, @no_grader = pagy(Section.where.missing(:assistants).all, page_param: :no_grader)
+    @pagy_course, @courses = pagy(Course.all, page_param: :course_page)
+    @pagy_user, @users = pagy(User.all, page_param: :user_page)
+    @pagy_pending, @pending_users = pagy(User.where(status: "pending").where.not(role: "student").all, page_param: :pending_page)
+    @pagy_assistant, @assistants = pagy(Assistant.all, page_param: :assistants)
+
+    @assistant = Assistant.new
+  end
 end
