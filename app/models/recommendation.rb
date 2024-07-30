be70a28337ce0c730419d8df6a9de2f9d124851f
Ryan Parand <parand.2@osu.edu>
2024-07-29 21:03:52 -0400

    comments and sources added

--- app/models/recommendation.rb
+++ app/models/recommendation.rb
@@ -1,3 +1,10 @@
+# The sources used for this file vary, including ChatGPT and Carmen resources:
+# https://www.youtube.com/playlist?list=PLlxmoA0rQ-Lx45j3D6da7-Iqvo5wtjKBm
+# https://www.youtube.com/playlist?list=PLS1QulWo1RIbNBXZAeVbkkHEj9zsEbXQK
+# https://www.railstutorial.org/book
+# https://www.youtube.com/watch?v=GY7Ps8fqGdc
+# https://chatgpt.com/
+
 class Recommendation < ApplicationRecord
   belongs_to :instructor, class_name: 'User'
   belongs_to :student, class_name: 'User', optional: true
